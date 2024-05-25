8afd784c60799128c678e99dad6584daf26b2081
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:43:00 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+let animals:string[]=["dog","cat","rabbit"];
+animals.forEach(animals =>{
+    console.log(`A ${animals} would make a great pet.`);
+})
+console.log("Any of these animals would make a great pet!");
